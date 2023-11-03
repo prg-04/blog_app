ed0ff9737114bf5bf9c6bd5ba6b68b92e4197bd1
prg-04 <karanjaevans95@gmail.com>
2023-11-03 21:40:12 +0300

    Add foreign keys

--- db/migrate/20231102091830_create_posts.rb
+++ db/migrate/20231102091830_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
-      t.integer :AuthorId
+      t.references :user, foreign_key: true
       t.string :Title
       t.text :Text
       t.integer :CommentsCounter, default: 0
--- db/migrate/20231102092318_create_comments.rb
+++ db/migrate/20231102092318_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
-      t.integer :UserId
-      t.integer :PostId
+      t.references :user, foreign_key: true
+      t.references :post, foreign_key: true
       t.text :Text
 
       t.timestamps
