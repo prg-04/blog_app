c1e2babc188f56421e16b8de984cb0e1be893d15
prg-04 <karanjaevans95@gmail.com>
2023-11-04 22:03:58 +0300

    Fix bug

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post, foreign_key: 'post_id'
   belongs_to :user, foreign_key: 'user_id'
-  has_many :likes
 
   after_create :increment_post_comments_counter
 
