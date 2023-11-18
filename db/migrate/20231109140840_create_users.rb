6eac585d3a684870eb4550f8ec9bed7da572bfb4
prg-04 <karanjaevans95@gmail.com>
2023-11-18 15:56:07 +0300

    Update user validations

--- db/migrate/20231109140840_create_users.rb
+++ db/migrate/20231109140840_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[7.1]
       t.string :name
       t.string :photo
       t.text :bio
-      t.integer :postCounter, :greater_than_or_equal_to: 0, default: 0
+      t.integer :postCounter, null:false, default: 0
 
       t.timestamps
     end
