e8e7cbba078e83572df09ed1e79600cbe33875e4
prg-04 <karanjaevans95@gmail.com>
2023-11-18 21:05:56 +0300

    Add form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,12 @@ class PostsController < ApplicationController
 
   def show
     @user = User.find_by_id(params[:user_id])
-    @post = Post.find_by_id(params[:id])
+    @post = @user.posts.find_by_id(params[:id])
+
+    # You might want to handle the case where @post is nil, for example:
+    return unless @post.nil?
+
+    flash[:alert] = 'Post not found'
+    redirect_to user_posts_path(@user)
   end
 end
