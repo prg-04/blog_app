404a7fbb32fd07d61a838d138667524fc868adf5
prg-04 <karanjaevans95@gmail.com>
2023-11-18 22:41:28 +0300

    Add forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 # config/routes.rb
 Rails.application.routes.draw do
-  get 'posts/create'
   root to: "main#index"
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new ] do
-      resources :comments, only: [:create]
+    resources :posts, only: [:index, :show, :new, :create ] do
+      resources :comments, only: [:new, :create]
     end
   end
 end
